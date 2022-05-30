73bdba74ef2d7154488b2d2c7093cf693ba65a48
kavantan <kavantan10@gmail.com>
2022-05-30 21:08:03 +0800

    Added Forum and Deals Routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import AppShell from "./components/AppShell";
 import PageHome from "./pages/PageHome";
 import PageLogIn from "./pages/PageLogIn";
 import PageProfile from "./pages/PageProfile";
+import PageDeals from "./pages/PageDeals";
+import PageForum from "./pages/PageForum";
 import { useAuth } from "./hooks/useAuth";
 import { Route, Routes } from "react-router-dom";
 
@@ -14,6 +16,8 @@ function App() {
       <Routes>
         <Route path="/" element={user ? <PageHome /> : <PageLogIn />} />
         <Route path="profile" element={<PageProfile />} />
+        <Route path="forum" element={<PageForum />} />
+        <Route path="deals" element={<PageDeals />} />
       </Routes>
     </div>
   );
