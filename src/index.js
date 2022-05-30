79ef08510effb6ea8352f84e85207b9fb73dc8da
kavantan <kavantan10@gmail.com>
2022-05-30 17:12:27 +0800

    Wrapped App in index.js with BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ProvideAuth } from "./hooks/useAuth";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <CssBaseline />
-    <ProvideAuth>
-      <App />
-    </ProvideAuth>
+    <BrowserRouter>
+      <CssBaseline />
+      <ProvideAuth>
+        <App />
+      </ProvideAuth>
+    </BrowserRouter>
   </React.StrictMode>
 );
