94eebc3c79381b8204c4891df4da64e467010e42
kavantan <kavantan10@gmail.com>
2022-05-30 17:25:27 +0800

    Added Profile Page placeholder and upgraded the placeholder pictures for log-in and home

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,12 @@
+import styles from "./Profile.module.css";
+import placeholder from "../../assets/profile-placeholder.png";
+
 const Profile = () => {
-  return <div>Profile</div>;
+  return (
+    <div>
+      <img src={placeholder} alt="Profile Page" className={styles.Center}></img>
+    </div>
+  );
 };
 
 export default Profile;
